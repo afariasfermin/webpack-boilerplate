a4c8cc0ab1c3c029a5025e482526d86e3b14c120
Alvaro Farias <afariasfermin@gmail.com>
2018-07-13 11:27:35 -0400

    Updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,32 @@
 const path = require('path');
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 
 module.exports = {
-    context: path.resolve(__dirname, "src"),
+    context: path.resolve(__dirname, 'src'),
+    devtool: 'inline-source-map',
     entry: './index.js',
     mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
     output: {
         filename: 'index.dist.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    plugins: [
+        new FriendlyErrorsWebpackPlugin()
+    ]
 }
