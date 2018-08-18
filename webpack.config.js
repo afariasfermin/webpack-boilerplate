5afd88b1d908ae06b6018430174474ddea03a2d4
Alvaro Farias <afariasfermin@gmail.com>
2018-08-17 22:08:50 -0300

    Use environment variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,40 @@
 const path = require('path');
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 
-module.exports = {
-    context: path.resolve(__dirname, 'src'),
-    devtool: 'inline-source-map',
-    entry: './index.js',
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                exclude: /node_modules/,
-                include: /src/,
-                test: /\.js$/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['env']
+module.exports = env => {
+    return {
+        context: path.resolve(__dirname, 'src'),
+        devtool: 'inline-source-map',
+        entry: './index.js',
+        mode: env.mode,
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: [
+                        'style-loader',
+                        'css-loader'
+                    ]
+                },
+                {
+                    exclude: /node_modules/,
+                    include: /src/,
+                    test: /\.js$/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['env']
+                        }
                     }
                 }
-            }
+            ]
+        },
+        output: {
+            filename: 'index.dist.js',
+            path: path.resolve(__dirname, 'dist')
+        },
+        plugins: [
+            new FriendlyErrorsWebpackPlugin()
         ]
-    },
-    output: {
-        filename: 'index.dist.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    plugins: [
-        new FriendlyErrorsWebpackPlugin()
-    ]
+    }
 }
